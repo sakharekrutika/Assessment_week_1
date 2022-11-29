3549b9548a63c929c06492efb7e6aa1c2a11d324
Krutika Sakhare <krusakha1@publicisgroupe.net>
2022-11-29 22:48:59 +0530

    Concatination file added

---
+++ src/test/java/Concatination/concat.java
@@ -0,0 +1,14 @@
+package Concatination;
+
+import org.testng.annotations.Test;
+
+public class concat {
+	@Test
+	public void concatination() {
+	String firstname="Krutika";
+	String lastname="Sakhare";
+	System.out.println("Hello "+firstname+" "+lastname);
+	
+	}
+	
+}
